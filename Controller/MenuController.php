ea0c6a746fc7d168ef9c458703ed9e88b490eb21
Michael Holm <hollo@hollo.dk>
2011-09-07 19:45:28 +0200

    Split top right, and top left menu from each other.

--- Controller/MenuController.php
+++ Controller/MenuController.php
@@ -20,6 +20,19 @@ class MenuController extends Controller
     ));
   }
 
+  public function topRightMenuAction()
+  {
+    $menu = array();
+
+    $event = new \Hollo\MenuBundle\Event\FilterMenuEvent($menu);
+    $this->get('event_dispatcher')->dispatch(\Hollo\MenuBundle\Event\Events::onTopRightMenuRender, $event);
+    $menu = $event->getMenu();
+
+    return $this->render('HolloMenuBundle:Menu:topRightMenu.html.twig', array(
+      'menu' => $menu
+    ));
+  }
+
   public function leftMenuAction()
   {
     $menu = array();
@@ -27,12 +40,6 @@ class MenuController extends Controller
     $event = new \Hollo\MenuBundle\Event\FilterMenuEvent($menu);
     $this->get('event_dispatcher')->dispatch(\Hollo\MenuBundle\Event\Events::onLeftMenuRender, $event);
     $menu = $event->getMenu();
-
-    if (preg_match("/admin\/([^\/]+)?.*$/", $this->getRequest()->getRequestUri(),$rtn)) {
-      if (isset($menu[$rtn[1]])) {
-        $menu[$rtn[1]]['active'] = 1;
-      }
-    }
 
     return $this->render('HolloMenuBundle:Menu:leftMenu.html.twig', array(
       'menu' => $menu
--- Event/Events.php
+++ Event/Events.php
@@ -5,5 +5,6 @@ namespace Hollo\MenuBundle\Event;
 final class Events
 {
   const onTopMenuRender = 'top_menu.render';
+  const onTopRightMenuRender = 'top_right_menu.render';
   const onLeftMenuRender = 'left_menu.render';
 }
